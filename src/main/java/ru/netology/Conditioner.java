6d401ce38df1c494636ac5cd7382e381384e268e
Maria Kochulanova <holamarymar@gmail.com>
2020-09-27 19:10:44 +0300

    Initial commit

---
+++ src/main/java/ru/netology/Conditioner.java
@@ -0,0 +1,10 @@
+package ru.netology;
+
+public class Conditioner {
+    private String name;
+    private int maxTemperature;
+    private int minTemperature;
+    private int currentTemperature;
+    private boolean on;
+    
+}
