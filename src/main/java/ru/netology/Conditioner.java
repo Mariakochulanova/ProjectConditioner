cebfab927315af71ab0786593651cc3e1b2fa7f1
Maria Kochulanova <holamarymar@gmail.com>
2020-09-30 19:41:53 +0300

    increaseCurrentTemperature added

--- src/main/java/ru/netology/Conditioner.java
+++ src/main/java/ru/netology/Conditioner.java
@@ -6,5 +6,44 @@ public class Conditioner {
     private int minTemperature;
     private int currentTemperature;
     private boolean on;
-    
+
+    public int setMaxTemperature(int maxTemperature) {
+        this.maxTemperature = maxTemperature;
+        return maxTemperature;
+    }
+
+    public void setMinTemperature(int minTemperature) {
+        this.minTemperature = minTemperature;
+    }
+
+    public int setCurrentTemperature(int currentTemperature) {
+        this.currentTemperature = currentTemperature;
+        return currentTemperature;
+    }
+
+    public int getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public int getMinTemperature() {
+        return minTemperature;
+    }
+
+    public int getCurrentTemperature() {
+        return currentTemperature;
+    }
+
+    public int increaseCurrentTemperature () {
+         int higherTemp = 0;
+
+        if(currentTemperature < maxTemperature){
+            higherTemp = currentTemperature + 1;
+        }
+        if (currentTemperature == maxTemperature){
+            higherTemp = maxTemperature;
+        }
+
+        return higherTemp;
+    }
+
 }
